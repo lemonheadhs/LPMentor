f620a744c0dbc1d106a6fc79808b76b589352132
lemonheadhs <744640996@qq.com>
2019-04-16 17:46:02 +0800

    update thoughts

--- script.fsx
+++ script.fsx
@@ -19,6 +19,25 @@ let EvernoteRaker_ds = {
     Parts = [
         Concept "Evernote Web Clipper - gather materials online"
         Concept "Evernote - store text materials as notes"
-        Concept "Tags? - a way to group a set of materials, so later we can stitch them together"
+        Concept "Tags or Commments? - a way to group a set of materials, so later we can stitch them together"
+        Concept "Evernote Webhook - get notified once all materials around one topic is ready"
     ]
 }
+
+let AzureTTS_ds = {
+    Topic = AzureTTS
+    Parts = [
+        Concept "Azure Function - get notified by Evernote Webhook, extract text contents from note, and feed them to TTL"
+        Concept "Azure Speech Service in Cognitive Services as TTL"
+    ]
+}
+
+let AudioStoreOnAzure_ds = {
+    Topic = AudioStoreOnAzure
+    Parts =[
+        Concept "Azure Blob Storage - a convenient service to store files"
+        Concept "How to retrieve audio files from Azure blobs?"
+    ]
+}
+
+

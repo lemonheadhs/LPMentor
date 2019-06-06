5293eb8816cffee36ff9c7a792f0810cc696290d
lemonheadhs <744640996@qq.com>
2019-06-06 17:17:12 +0800

    add console tool for renew authToken; update workflow

---
+++ src/LPMentor.ConsoleTool/Program.fs
@@ -0,0 +1,78 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FSharp.Data
+open System.IO
+open System.Reflection
+open Microsoft.Azure.Storage
+open Microsoft.Azure.Storage.Queue
+
+open LPMentor.Core.WebhookFn.EvernoteAuth
+
+
+let cwd = Assembly.GetEntryAssembly().Location |> Path.GetDirectoryName
+
+[<Literal>]
+let configSample = """{
+    "azureStorageConnStr": "UseDevelopmentStorage=true"
+}"""
+type Config = JsonProvider<configSample>
+let config = Path.Combine(cwd, "appsettings.json") |> Config.Load
+
+
+module Queue = begin
+    let callbackQueue =
+        CloudStorageAccount.Parse(config.AzureStorageConnStr)
+                           .CreateCloudQueueClient()
+                           .GetQueueReference("LPAuthCallback")
+end
+
+module Table = begin
+    open Microsoft.Azure.Cosmos.Table
+
+    let credTable =
+        CloudStorageAccount.Parse(config.AzureStorageConnStr)
+                           .CreateCloudTableClient()
+                           .GetTableReference("LPCredential")
+
+    type CredEntity (partition:string, rowId:string) =
+        inherit TableEntity (partition, rowId)
+        member val Host = "" with get, set
+        member val EdamUserId = "" with get, set
+        member val NoteStoreUrl = "" with get, set
+        member val WebApiUrlPrefix = "" with get, set
+        member val AuthenticationToken = "" with get, set
+        member val ExpirationDate = DateTime() with get, set
+    with
+        static member ModifyWith (ni: LENCredentials) (e: CredEntity) =
+            e.Host <- ni.Host
+            e.EdamUserId <- ni.EdamUserId
+            e.NoteStoreUrl <- ni.NoteStoreUrl
+            e.WebApiUrlPrefix <- ni.WebApiUrlPrefix
+            e.AuthenticationToken <- ni.AuthenticationToken
+            e.ExpirationDate <- ni.ExpirationDate
+            e
+        static member Save (ni: LENCredentials) =
+            let e = 
+                CredEntity("v1", SessionHost)
+                |> CredEntity.ModifyWith (ni)
+            let table = credTable
+            e
+            |> TableOperation.InsertOrReplace
+            |> table.ExecuteAsync
+end
+
+[<EntryPoint>]
+let main argv =
+    printfn "Console tool for LPMentor renew authToken"
+    authenticateToEvernote() |> function
+    | None ->
+        printfn "fail to authenticate to Evernote.."
+    | Some cred ->
+        Table.CredEntity.Save(cred).Wait()
+        DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")
+        |> CloudQueueMessage
+        |> Queue.callbackQueue.AddMessage
+        printfn "Refreshed Cred saved!"
+    printfn "Done.."
+    0 // return an integer exit code
--- src/LPMentor.Durable/Workflow.fs
+++ src/LPMentor.Durable/Workflow.fs
@@ -2,45 +2,80 @@ module LPMentor.Durable.Workflow
 
 open System
 open Microsoft.Azure.WebJobs
-open Microsoft.WindowsAzure.Storage.Blob
 open DurableFunctions.FSharp
 open FSharp.Control.Tasks.V2
 open System.Threading.Tasks
-open System.IO
 
 open LPMentor.Core.WebhookFn.Evernote
+open LPMentor.Core.WebhookFn.EvernoteAuth
 open LPMentor.Core.TTSFn.AzureSpeech
 open LPMentor.Core.Models
 open Storage.Blob
 open Storage.Table
 open System.Collections.Concurrent
+open Evernote.EDAM.NoteStore
+open FSharp.Control.Tasks
 
 let (<!+>) activityName f = Activity.define activityName f
 let (<!->) activityName f = Activity.defineTask activityName f
 let (<**>) activity p = Activity.call activity p
+
+let inline (>>=) (m: Task<'a>) (f: 'a -> Task<'b>) =
+    task {
+        let! a = m
+        return! (f a)
+    }
 
 [<AutoOpen>]
 module Activities = begin
     [<AutoOpen>]
     module ActivityFuncImpl = begin
         let internal credCache = ConcurrentDictionary<string, CredEntity>()
-        let credExsists_ () =
+        let ofValidCred (cred: CredEntity) = 
+            cred.ExpirationDate.AddHours(-3.) < DateTime.Now |> function
+            | true -> Some cred
+            | false -> None
+        let getValidCachedCred () =
             credCache.TryGetValue("cred") |> function
-            | true, cred -> cred.ExpirationDate.AddHours(-3.) < DateTime.Now
-            | false, _ -> false
-            
+            | true, cred -> ofValidCred cred
+            | _ -> None
+        let internal noteStoreCache = ConcurrentDictionary<string, NoteStore.Client>()
+        let getCachedNoteStore (cred: CredEntity) =
+            noteStoreCache.GetOrAdd(cred.NoteStoreUrl, noteStoreClient)
 
+        type FetchNoteError = | CredExpired | NoteNotFound
+        
         let fetchNote_ noteGuid =
-            FetchNoteContent noteGuid
-            |> Option.map (fun (content, metadata) ->
-                                let noteInfo: NoteInfo = {
-                                    Text = content
-                                    Topic = metadata.Topic
-                                    Section = metadata.Section
-                                    Order = metadata.Order
-                                    Lang = metadata.Lang
-                                }
-                                noteInfo)                
+            task {
+                let! credOpt =
+                    getValidCachedCred() |> function
+                    | Some cred -> Task.FromResult (Some cred)
+                    | None -> 
+                        CredEntity.TryGet() 
+                        >>= (   Option.bind ofValidCred
+                             >> Option.map (fun validCred ->
+                                                credCache.AddOrUpdate("cred", validCred, (fun s c -> validCred)))
+                             >> Task.FromResult)
+                let contentResult =
+                    credOpt
+                    |> Result.ofOption CredExpired
+                    |> Result.bind (
+                        fun cred ->
+                            let noteStore = getCachedNoteStore cred
+                            FetchNoteContentWith noteStore cred.AuthenticationToken noteGuid
+                            |> Result.ofOption NoteNotFound)
+                    |> Result.map (
+                        fun (content, metadata) ->
+                            let noteInfo: NoteInfo = {
+                                Text = content
+                                Topic = metadata.Topic
+                                Section = metadata.Section
+                                Order = metadata.Order
+                                Lang = metadata.Lang
+                            }
+                            noteInfo)  
+                return contentResult
+            }        
 
         let genAudio_ (noteInfo: NoteInfo) =
             let ssml = SSML.genDefaultSSML noteInfo.Lang noteInfo.Text
@@ -68,24 +103,20 @@ module Activities = begin
             }
     end
 
-    let fetchCred = "FetchCred" <!+> fetchCred_
-    let fetchNote = "FetchNote" <!+> fetchNote_
+    let fetchNote = "FetchNote" <!-> fetchNote_
     let genAudio = "GenAudio" <!-> genAudio_
     let storeAudioInfo = "StoreAudioInfo" <!-> storeAudioInfo_
 end
 
 
 let workflow instanceId (webhookParam: WebhookParam) = orchestrator {
-    let! credOpt = fetchCred <**> ()
-    let mutable cred: CredEntity = CredEntity("", "")
     
-    let! test =
-        if credOpt |> Option.isSome then
-            cred <- Option.get credOpt
-            Orchestrator.ret ()
-        else
-            Orchestrator.ret ()
-    let! optionNoteInfo = fetchNote <**> webhookParam.Guid_
+    let! firstTry = fetchNote <**> webhookParam.Guid_
+    firstTry |> function
+    | Some noteInfo -> Orchestrator.ret (Some noteInfo)
+    | None ->
+        let maxWaitDuration = TimeSpan.FromDays 7.
+        (Orchestrator.waitForEvent maxWaitDuration "RenewAuth")
     
     if Option.isSome optionNoteInfo then
         let noteInfo = Option.get optionNoteInfo

a60254a902eac4a1ef0aa26aa62e9184ca7c5273
lemonheadhs <744640996@qq.com>
2019-06-20 18:04:49 +0800

    add SAFE stack web app project

--- build.fsx
+++ build.fsx
@@ -1,11 +1,10 @@
-#r "paket:
-nuget Fake.Core.Target
-nuget Fake.IO.FileSystem
-nuget Fake.IO.Zip
-nuget Fake.Azure.Kudu
-nuget Fake.DotNet.Cli
-nuget FSharp.Data //"
-#load "./.fake/build.fsx/intellisense_lazy.fsx"
+#r "paket: groupref build //"
+#load "./.fake/build.fsx/intellisense.fsx"
+
+#if !FAKE
+#r "netstandard"
+#r "Facades/netstandard" // https://github.com/ionide/ionide-vscode-fsharp/issues/839#issuecomment-396296095
+#endif
 
 open System.IO
 open FSharp.Data
@@ -60,7 +59,124 @@ Target.create "Deploy" (fun _ ->
            printfn "App Service %s deployed" p.Name)
     printfn "All App Services are updated!")
 
+
+open System
+
+open Fake.Core
+open Fake.DotNet
+open Fake.IO
+
+let webProjFolder = Path.getFullName "./src/LPMentor.Web"
+let serverPath = Path.getFullName "./src/LPMentor.Web/Server"
+let clientPath = Path.getFullName "./src/LPMentor.Web/Client"
+let clientDeployPath = Path.combine clientPath "deploy"
+let deployDir = Path.getFullName "./deploy"
+
+let release = ReleaseNotes.load (Path.combine webProjFolder "RELEASE_NOTES.md")
+
+let platformTool tool winTool =
+    let tool = if Environment.isUnix then tool else winTool
+    match ProcessUtils.tryFindFileOnPath tool with
+    | Some t -> t
+    | _ ->
+        let errorMsg =
+            tool + " was not found in path. " +
+            "Please install it and make sure it's available from your path. " +
+            "See https://safe-stack.github.io/docs/quickstart/#install-pre-requisites for more info"
+        failwith errorMsg
+
+let nodeTool = platformTool "node" "node.exe"
+let yarnTool = platformTool "yarn" "yarn.cmd"
+
+let runTool cmd args workingDir =
+    let arguments = args |> String.split ' ' |> Arguments.OfArgs
+    Command.RawCommand (cmd, arguments)
+    |> CreateProcess.fromCommand
+    |> CreateProcess.withWorkingDirectory workingDir
+    |> CreateProcess.ensureExitCode
+    |> Proc.run
+    |> ignore
+
+let runDotNet cmd workingDir =
+    let result =
+        DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
+    if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
+
+let openBrowser url =
+    //https://github.com/dotnet/corefx/issues/10361
+    Command.ShellCommand url
+    |> CreateProcess.fromCommand
+    |> CreateProcess.ensureExitCodeWithMessage "opening browser failed"
+    |> Proc.run
+    |> ignore
+
+
+Target.create "CleanWeb" (fun _ ->
+    [ deployDir
+      clientDeployPath ]
+    |> Shell.cleanDirs
+)
+
+Target.create "InstallClient" (fun _ ->
+    printfn "Node version:"
+    runTool nodeTool "--version" webProjFolder
+    printfn "Yarn version:"
+    runTool yarnTool "--version" webProjFolder
+    runTool yarnTool "install --frozen-lockfile" webProjFolder
+)
+
+Target.create "BuildWeb" (fun _ ->
+    runDotNet "build" serverPath
+    Shell.regexReplaceInFileWithEncoding
+        "let app = \".+\""
+       ("let app = \"" + release.NugetVersion + "\"")
+        System.Text.Encoding.UTF8
+        (Path.combine clientPath "Version.fs")
+    runTool yarnTool "webpack-cli -p" webProjFolder
+)
+
+Target.create "Run" (fun _ ->
+    let server = async {
+        runDotNet "watch run" serverPath
+    }
+    let client = async {
+        runTool yarnTool "webpack-dev-server" webProjFolder
+    }
+    let browser = async {
+        do! Async.Sleep 5000
+        openBrowser "http://localhost:8080"
+    }
+
+    let vsCodeSession = Environment.hasEnvironVar "vsCodeSession"
+    let safeClientOnly = Environment.hasEnvironVar "safeClientOnly"
+
+    let tasks =
+        [ if not safeClientOnly then yield server
+          yield client
+          if not vsCodeSession then yield browser ]
+
+    tasks
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> ignore
+)
+
+
+
+
+
+
 open Fake.Core.TargetOperators
+
+"CleanWeb"
+    ==> "InstallClient"
+    ==> "BuildWeb"
+
+
+"CleanWeb"
+    ==> "InstallClient"
+    ==> "Run"
+
 
 "Clean" 
     ==> "Build" 

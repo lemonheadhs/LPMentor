52ff2301b73faee64453b109eaf17b26126c184e
lemonheadhs <744640996@qq.com>
2019-08-06 16:49:46 +0800

    update

--- src/LPMentor.Web/src/Client/Client.fs
+++ src/LPMentor.Web/src/Client/Client.fs
@@ -16,104 +16,61 @@ open Search
 // in this case, we are keeping track of a counter
 // we mark it as optional, because initially it will not be available from the client
 // the initial value will be requested from server
-type Model = { Counter: Counter option }
+type Model = { 
+    Lessons: Lecture.Model
+    Search: Search.Model
+    NextToken: TableToken
+}
 
 // The Msg type defines what events/actions can occur while the application is running
 // the state of the application changes *only* in reaction to these events
 type Msg =
-| Increment
-| Decrement
-| InitialCountLoaded of Counter
+| Lesson of Lecture.Msg * TableToken option
+| Search of Search.Msg
 
-module Server =
-
-    open Shared
-    open Fable.Remoting.Client
-
-    /// A proxy you can use to talk to server directly
-    let api : ICounterApi =
-      Remoting.createApi()
-      |> Remoting.withRouteBuilder Route.builder
-      |> Remoting.buildProxy<ICounterApi>
-let initialCounter = Server.api.initialCounter
 
 // defines the initial state and initial command (= side-effect) of the application
 let init () : Model * Cmd<Msg> =
-    let initialModel = { Counter = None }
-    let loadCountCmd =
-        Cmd.OfAsync.perform initialCounter () InitialCountLoaded
-    initialModel, loadCountCmd
+    
+    let lessonsModel, cmds1 = Lecture.init ()
+    let searchModel, cmds2 = Search.init ()
+    { Lessons = lessonsModel
+      Search = searchModel
+      NextToken = TableToken.empty },
+    Cmd.map (fun l -> Msg.Lesson (l, None)) cmds1 @ Cmd.map Search cmds2
 
 // The update function computes the next state of the application based on the current state and the incoming events/messages
 // It can also run side-effects (encoded as commands) like calling the server via Http.
 // these commands in turn, can dispatch messages to which the update function will react.
-let update (msg : Msg) (currentModel : Model) : Model * Cmd<Msg> =
-    match currentModel.Counter, msg with
-    | Some counter, Increment ->
-        let nextModel = { currentModel with Counter = Some { Value = counter.Value + 1 } }
-        nextModel, Cmd.none
-    | Some counter, Decrement ->
-        let nextModel = { currentModel with Counter = Some { Value = counter.Value - 1 } }
-        nextModel, Cmd.none
-    | _, InitialCountLoaded initialCount->
-        let nextModel = { Counter = Some initialCount }
-        nextModel, Cmd.none
-    | _ -> currentModel, Cmd.none
+let update (msg : Msg) (currModel : Model) : Model * Cmd<Msg> =
+    match msg with
+    | Lesson (lessonMsg, tokenOption) ->
+        let m, c = Lecture.update lessonMsg currModel.Lessons
+        { currModel with Lessons = m }, 
+        Cmd.map Msg.Lesson c
 
+    | Search searchMsg ->
+        let cmd =
+            let chooseReqFn = function        
+                | Some term -> LessonAPIClient.searchTopic term
+                | None      -> LessonAPIClient.init
+            match searchMsg with
+            | ConfirmSearch strOption ->
+                let reqFn = chooseReqFn strOption
+                Cmd.OfAsync.perform reqFn TableToken.empty
+                    (fun (ls, t) ->
+                        ( ls |> List.ofArray |> Lecture.Full,
+                          Some t) |> Msg.Lesson)
+            | RequestNext ->
+                let reqNextFn = chooseReqFn currModel.Search.SearchTerm
+                Cmd.OfAsync.perform reqNextFn currModel.NextToken
+                    (fun (ls, t) ->
+                        ( ls |> List.ofArray |> Lecture.Append,
+                          Some t) |> Msg.Lesson)
 
-let safeComponents =
-    let components =
-        span [ ]
-           [ a [ Href "https://github.com/SAFE-Stack/SAFE-template" ]
-               [ str "SAFE  "
-                 str Version.template ]
-             str ", "
-             a [ Href "https://github.com/giraffe-fsharp/Giraffe" ] [ str "Giraffe" ]
-             str ", "
-             a [ Href "http://fable.io" ] [ str "Fable" ]
-             str ", "
-             a [ Href "https://elmish.github.io" ] [ str "Elmish" ]
-             str ", "
-             a [ Href "https://fulma.github.io/Fulma" ] [ str "Fulma" ]
-             str ", "
-             a [ Href "https://zaid-ajaj.github.io/Fable.Remoting/" ] [ str "Fable.Remoting" ]
-
-           ]
-
-    span [ ]
-        [ str "Version "
-          strong [ ] [ str Version.app ]
-          str " powered by: "
-          components ]
-
-let show = function
-| { Counter = Some counter } -> string counter.Value
-| { Counter = None   } -> "Loading..."
-
-let button txt onClick =
-    Button.button
-        [ Button.IsFullWidth
-          Button.Color IsPrimary
-          Button.OnClick onClick ]
-        [ str txt ]
-
-let oldview (model : Model) (dispatch : Msg -> unit) =
-    div []
-        [ Navbar.navbar [ Navbar.Color IsPrimary ]
-            [ Navbar.Item.div [ ]
-                [ Heading.h2 [ ]
-                    [ str "SAFE Template" ] ] ]
-
-          Container.container []
-              [ Content.content [ Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
-                    [ Heading.h3 [] [ str ("Press buttons to manipulate counter: " + show model) ] ]
-                Columns.columns []
-                    [ Column.column [] [ button "-" (fun _ -> dispatch Decrement) ]
-                      Column.column [] [ button "+" (fun _ -> dispatch Increment) ] ] ]
-
-          Footer.footer [ ]
-                [ Content.content [ Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
-                    [ safeComponents ] ] ]
+        let m, c = Search.update searchMsg currModel.Search
+        { currModel with Search = m },
+        Cmd.map Msg.Search c @ cmd
 
 let appNav =
     Navbar.navbar [ Navbar.HasShadow ]
--- src/LPMentor.Web/src/Client/Search.fs
+++ src/LPMentor.Web/src/Client/Search.fs
@@ -11,16 +11,15 @@ open Fable.Remoting.Client
 open Shared
 open Microsoft.FSharp.Control
 open System
+open Browser.Types
 
 type Model = {
     SearchTerm: string option
-    ContinuationToken: TableToken
 }
 
 type Msg =
 | ConfirmSearch of string option
 | RequestNext
-| NextToken of TableToken
 
 type Uplink = {
     LessonChannel: Lecture.Msg -> unit
@@ -31,43 +30,29 @@ let LessonAPIClient =
     |> Remoting.withRouteBuilder Route.builder
     |> Remoting.buildProxy<ILessonSearchApi>
 
-let init (uplink: Uplink) () =
-    { SearchTerm = None; ContinuationToken = TableToken.empty },
-    Cmd.OfAsync.perform LessonAPIClient.init TableToken.empty 
-        (fun (ls, t) -> 
-            ls |> List.ofArray |> Lecture.Full |> uplink.LessonChannel
-            NextToken t)
+let init () =
+    { SearchTerm = None },
+    Cmd.none
 
-let update (uplink: Uplink) (msg: Msg) (currModel: Model) : Model * Cmd<Msg>=
-    let chooseReqFn = function        
-        | Some term -> LessonAPIClient.searchTopic term
-        | None      -> LessonAPIClient.init
+let update (msg: Msg) (currModel: Model) : Model * Cmd<Msg>=    
     match msg with
     | ConfirmSearch strOption ->
-        let reqFn = chooseReqFn strOption
         { currModel with SearchTerm = strOption },
-        Cmd.OfAsync.perform reqFn TableToken.empty
-            (fun (ls, t) ->
-                ls |> List.ofArray |> Lecture.Full |> uplink.LessonChannel
-                NextToken t)
+        Cmd.none
     | RequestNext ->
-        let reqNextFn = chooseReqFn currModel.SearchTerm
-        currModel,
-        Cmd.OfAsync.perform reqNextFn currModel.ContinuationToken
-            (fun (ls, t) ->
-                ls |> List.ofArray |> Lecture.Append |> uplink.LessonChannel
-                NextToken t)
-    | NextToken t ->
-        { currModel with ContinuationToken = t },
-        Cmd.Empty
+        currModel, Cmd.none
 
 let view (model: Model) (dispatch: Msg -> unit) =
     // { new IObservable<string> with
     //     member _.Subscribe() }
+    let test (ke: KeyboardEvent) =
+        Browser.Dom.console.log ke
+        
     let inputControl =
         Control.div [ Control.HasIconLeft; Control.HasIconRight ]
             [ Input.input [ Input.Option.Size IsLarge
-                            Input.Option.Type Input.IInputType.Search ]
+                            Input.Option.Type Input.IInputType.Search
+                            Input.Option.Props [ OnKeyUp (test) ] ]
               Icon.icon [ Icon.Option.IsLeft; Icon.Option.Size IsMedium ]
                 [ Fa.i [ Fa.Solid.Search ] [ ] ]
               Icon.icon [ Icon.Option.IsRight; Icon.Option.Size IsMedium ]
